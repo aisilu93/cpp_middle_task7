5d7ada26aedf22672ec962d92802b1bc0ae467e5
aisilu93 <aisilu93@yandex.ru>
2026-07-04 16:11:57 +0000

    Подключение клиента; разбор заголовков, тесты

--- src/headers.cpp
+++ src/headers.cpp
@@ -1,21 +1,59 @@
 #include "headers.h"
 
+#include <charconv>
+#include <optional>
 #include <ranges>
 #include <string_view>
+#include <utility>
 
 using namespace std::string_view_literals;
 
 using Callback = std::function<void(std::string_view, std::string_view)>;
 
+void iterHeaders(std::string_view req, Callback &&callback) {
+    for (const auto &header : std::views::split(req, "\r\n"sv)) {
+        auto parts = header | std::ranges::views::split(": "sv) | std::ranges::views::transform([](auto &&rng) {
+                         return std::string_view(&*rng.begin(), std::ranges::distance(rng));
+                     }) |
+                     std::ranges::to<std::vector<std::string_view>>();
+        if (parts.size() != 2)
+            continue;
 
-void iterHeaders(std::string_view req, Callback&& callback) {
-  // code here
+        callback(parts[0], parts[1]);
+    }
 }
 
 std::pair<std::string, std::string> findHostPort(std::string_view req) {
-  // code here
+    std::string host;
+    std::string port;
+
+    iterHeaders(req, [&](std::string_view name, std::string_view value) {
+        if (name == "Host") {
+            auto parts = value | std::ranges::views::split(':') | std::ranges::views::transform([](auto &&rng) {
+                             return std::string_view(&*rng.begin(), std::ranges::distance(rng));
+                         }) |
+                         std::ranges::to<std::vector<std::string_view>>();
+            if (parts.empty())
+                return;
+
+            host = parts[0];
+            if (parts.size() > 1)
+                port = parts[1];
+            else
+                port = "80";
+        }
+    });
+    return std::make_pair(host, port);
 }
 
 std::optional<size_t> findContentLength(std::string_view rsp) {
-  // code here
+    std::optional<size_t> result = std::nullopt;
+    iterHeaders(rsp, [&](std::string_view name, std::string_view value) {
+        if (name == "Content-Length") {
+            size_t len = 0;
+            if (std::from_chars(value.begin(), value.end(), len).ec == std::errc())
+                result = len;
+        }
+    });
+    return result;
 }
--- tests/headers_test.cpp
+++ tests/headers_test.cpp
@@ -1,38 +1,85 @@
-#include <gtest/gtest.h>
 #include "headers.h"
+#include <gtest/gtest.h>
 
 TEST(iterHeaders, Empty) {
-  // code here
+    std::string_view req("");
+    int call_count = 0;
+    iterHeaders(req, [&](std::string_view, std::string_view) { call_count++; });
+    EXPECT_EQ(call_count, 0);
 }
 
 TEST(iterHeaders, SkipRequestLine) {
-  // code here
+    std::string_view req("GET /index.html HTTP/1.1\r\nHost: example.com\r\n");
+    int call_count = 0;
+    iterHeaders(req, [&](std::string_view, std::string_view) { call_count++; });
+    EXPECT_EQ(call_count, 1);
 }
 
 TEST(iterHeaders, SingleHeader) {
-  // code here
+    std::string_view req("Host: example.com\r\n");
+    std::vector<std::pair<std::string, std::string>> headers;
+    iterHeaders(req, [&](std::string_view name, std::string_view value) { headers.emplace_back(name, value); });
+    EXPECT_EQ(headers.size(), 1);
+    EXPECT_EQ(headers[0].first, "Host");
+    EXPECT_EQ(headers[0].second, "example.com");
 }
 
 TEST(iterHeaders, MultipleHeaders) {
-  // code here
+    std::string_view req("GET /index.html HTTP/1.1\r\nHost: example.com\r\nAccept-Encoding: gzip\r\n");
+    std::vector<std::pair<std::string, std::string>> headers;
+    iterHeaders(req, [&](std::string_view name, std::string_view value) { headers.emplace_back(name, value); });
+    EXPECT_EQ(headers.size(), 2);
+    EXPECT_EQ(headers[0].first, "Host");
+    EXPECT_EQ(headers[0].second, "example.com");
+    EXPECT_EQ(headers[1].first, "Accept-Encoding");
+    EXPECT_EQ(headers[1].second, "gzip");
 }
 
 TEST(iterHeaders, MultipleSameHeaders) {
-  // code here
+    std::string_view req("GET /index.html HTTP/1.1\r\nHost: example.com\r\nHost: example.com\r\n");
+    int call_count = 0;
+    iterHeaders(req, [&](std::string_view, std::string_view) { call_count++; });
+    EXPECT_EQ(call_count, 2);
 }
 
 TEST(findHostPort, Simple) {
-  // code here
+    std::string_view req("GET /index.html HTTP/1.1\r\nHost: example.com:12345\r\n");
+    auto res = findHostPort(req);
+    EXPECT_EQ(res.first, "example.com");
+    EXPECT_EQ(res.second, "12345");
 }
 
 TEST(findHostPort, NoHost) {
-  // code here
+    std::string_view req("GET /index.html HTTP/1.1\r\nAccept-Encoding: gzip\r\n");
+    auto res = findHostPort(req);
+    EXPECT_TRUE(res.first.empty());
+    EXPECT_TRUE(res.second.empty());
+}
+
+TEST(findHostPort, NoPort) {
+    std::string_view req("GET /index.html HTTP/1.1\r\nHost: example.com\r\nAccept-Encoding: gzip\r\n");
+    auto res = findHostPort(req);
+    EXPECT_EQ(res.first, "example.com");
+    EXPECT_EQ(res.second, "80");
 }
 
 TEST(findContentLength, Simple) {
-  // code here
+    std::string_view req(
+        "GET /index.html HTTP/1.1\r\nHost: example.com:12345\r\nAccept-Encoding: gzip\r\nContent-Length: 5678\r\n");
+    auto res = findContentLength(req);
+    EXPECT_TRUE(res.has_value());
+    EXPECT_EQ(res.value(), 5678);
 }
 
 TEST(findContentLength, NoContentLength) {
-  // code here
+    std::string_view req("GET /index.html HTTP/1.1\r\nHost: example.com:12345\r\nAccept-Encoding: gzip\r\n");
+    auto res = findContentLength(req);
+    EXPECT_FALSE(res.has_value());
+}
+
+TEST(findContentLength, InvalidLength) {
+    std::string_view req(
+        "GET /index.html HTTP/1.1\r\nHost: example.com:12345\r\nContent-Length: abc\r\nAccept-Encoding: gzip\r\n");
+    auto res = findContentLength(req);
+    EXPECT_FALSE(res.has_value());
 }
